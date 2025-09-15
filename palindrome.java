75f62993f208b424c0e01b3a0d01dcba400d602a
sourishnandi153-dev <sourishnandi153@gmail.com>
2025-09-15 16:24:26 +0530

    palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PalindromeNumber {
+public class palindrome {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
